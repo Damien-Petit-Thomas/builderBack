83435dc0862c7ad2ceab8590ce7a32b12cacb173
Damien-Petit-Thomas <damien.petit.thomas@gmail.com>
2023-05-16 10:41:05 +0200

    connectiong db

--- app/config/db.js
+++ app/config/db.js
@@ -1,7 +1,12 @@
 const debug = require('debug')('SQL:log');
 const { Pool } = require('pg');
+require('dotenv').config();
 
-const pool = new Pool();
+const pool = new Pool(
+  {
+    connectionString: process.env.DATABASE_URL,
+  },
+);
 
 module.exports = {
 
