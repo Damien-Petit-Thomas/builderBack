2ef4b48c1d7a7edc2d13db29554898dced323db5
Damien-Petit-Thomas <damien.petit.thomas@gmail.com>
2023-05-24 10:25:33 +0200

    modif log

--- app/helpers/apiDocs.js
+++ app/helpers/apiDocs.js
@@ -6,17 +6,12 @@ const options = {
     title: 'O\'Builder',
     description: 'O\'Builder API l\'API pour les dresseurs de pokémon',
   },
-  security: {
-    BasicAuth: {
-      type: 'http',
-      scheme: 'basic',
-    },
-    baseDir: __dirname,
-    filesPattern: ['./*.js'],
-    swaggerUIPath: process.env.API_PATH,
-    exposeApiDocs: true,
-    apiDocsPath: '/api-docs',
-  },
+
+  baseDir: __dirname,
+  filesPattern: ['./*.js'],
+  swaggerUIPath: process.env.API_PATH,
+  exposeApiDocs: true,
+  apiDocsPath: '/api-docs',
 };
 
 module.exports = (app) => expressJSDocSwagger(app)(options);
