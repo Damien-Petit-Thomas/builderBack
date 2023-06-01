a7bd0733d821e2336b19affd688995c7c60dfc09
Damien-Petit-Thomas <damien.petit.thomas@gmail.com>
2023-06-01 14:18:11 +0200

    change preformate return

--- app/utils/pokemon.utils/preformatePokemon.js
+++ app/utils/pokemon.utils/preformatePokemon.js
@@ -1,21 +1,28 @@
 //* this utils function is to retrieve the pokemon data from the db and and pass it to the
 //* buildFormatedPokemonFromDb function then add the pokemon to the cache
 
-const { type } = require('../../models/index');
+const { type } = require('../../models');
 const buildPokemonObjectFromPokeDb = require('./buildFormatedPokemonFromDb');
-const CacheServer = require('../cache/pokemon.cache');
 
-const cache = CacheServer.getInstance();
+const CacheType = require('../cache/type.cache');
+
+const cacheType = CacheType.getInstance();
 
 module.exports = async (pokemon) => {
   const { type1, type2 } = pokemon;
   const pokemonTypes = [type1, type2].filter(Boolean);
+  const buildPokemons = await Promise.all(
+    pokemonTypes.map(async (typeId) => {
+      const cachedType = cacheType.get(typeId);
+      if (cachedType) {
+        return buildPokemonObjectFromPokeDb(pokemon, cachedType);
+      }
+      const typesData = await Promise.all(pokemonTypes.map((id) => type.findByPk(id)));
+      cacheType.set(typeId, typesData, cacheType.TTL);
+      const formatedPokemon = buildPokemonObjectFromPokeDb(pokemon, typesData);
 
-  const typesData = await Promise.all(pokemonTypes.map((typeId) => type.findByPk(typeId)));
-
-  const formatedPokemon = buildPokemonObjectFromPokeDb(pokemon, typesData);
-
-  cache.set(pokemon.id, formatedPokemon, cache.TTL);
-
-  return formatedPokemon;
+      return formatedPokemon;
+    }),
+  );
+  return { buildPokemons };
 };
